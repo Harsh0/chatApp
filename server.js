a9edf15ea965e99fae5384b5f3a4565139f34cc7
Harsh <harsh.singhal040@gmail.com>
2017-02-12 16:01:20 +0530

    'fd'

--- server.js
+++ server.js
@@ -6,7 +6,6 @@ const passport = require('passport');
 
 app.set('port',process.env.port||3000);
 app.use(express.static('public'));
-app.use(express.static('node_modules/babel-standalone'));
 app.set('view engine','ejs');
 
 app.use(ChatApp.session);
